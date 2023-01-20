7dc2d53814463ea8afd6c5b7a3379eb4ab0e6135
djxxr <71663821+Djxxr@users.noreply.github.com>
2023-01-20 02:50:27 +0100

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,6 @@
+const discord = document.getElementById("discord");
+const discordFrame = document.getElementById("discord-frame");
+
+discord.addEventListener("mouseover", function(){
+  discordFrame.src = "https://discord.com/";
+});
